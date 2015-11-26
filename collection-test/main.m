934907bb1d5e6639e5b2f104aefd5d1a935b0be3
Sean Kemper <skemper@groupon.com>
2015-11-26 00:01:58 -0800

    parse and collection view

---
+++ collection-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  collection-test
+//
+//  Created by Sean Kemper on 11/25/15.
+//  Copyright © 2015 Sean Kemper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
